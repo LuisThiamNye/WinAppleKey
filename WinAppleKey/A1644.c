5ff9f168a39dde9d41a002d8162c7ef547688279
Luis Thiam-Nye <luis.tn@protonmail.ch>
2025-01-27 03:08:35 +0000

    Properly support multiple simultaneous keys with Fn.

--- WinAppleKey/A1644.c
+++ WinAppleKey/A1644.c
@@ -1,122 +1,115 @@
 #include "driver.h"
 
-BOOLEAN g_FakeFnActive = 0;
+BOOLEAN g_FnKeyActive = 0;
 
-void ProcessA1644Buffer(BYTE* buf, ULONG size)
+typedef struct KbdBuf KbdBuf;
+struct KbdBuf {
+	BYTE modifiers;
+	BYTE _;
+	BYTE keys[6]; // The keyboard will only recognise six normal keys simultaneously; any further pressed keys are ignored.
+	BYTE special_key;
+};
+
+void ProcessA1644Buffer(BYTE* buf_, ULONG size)
 {
-	BYTE* pModifier = &buf[0];
-	BYTE* pKey1 = &buf[2];
-	BYTE* pKey2 = &buf[3];
-	BYTE* pKey3 = &buf[4];
-	BYTE* pKey4 = &buf[5];
-	BYTE* pKey5 = &buf[6];
-	BYTE* pKey6 = &buf[7];
-	BYTE* pSpecialKey = &buf[8];
+	KbdBuf* buf = buf_;
 
 	// SwapFnCtrl mode
 	//if (g_dwSwapFnCtrl)
 	//{
 	//	// Physical LCtrl pressed
-	//	if (*pModifier & HidLCtrlMask)
+	//	if (buf->modifiers & HidLCtrlMask)
 	//	{
-	//		if(!*pKey1) // And is it pressed alone?
-	//			g_FakeFnActive = TRUE;
+	//		if(!buf->keys[0]) // And is it pressed alone?
+	//			g_FnKeyActive = TRUE;
 
-	//		*pModifier &= ~HidLCtrlMask; // Clear LCtrl modifier
+	//		buf->modifiers &= ~HidLCtrlMask; // Clear LCtrl modifier
 	//	}
 	//	else // Physical LCtrl not pressed
 	//	{
-	//		if (!*pKey1) // Only unset g_FakeFnActive when there is no other key still being pressed
-	//			g_FakeFnActive = FALSE;
+	//		if (!buf->keys[0]) // Only unset g_FnKeyActive when there is no other key still being pressed
+	//			g_FnKeyActive = FALSE;
 	//	}
 
 	//	// Physical Fn pressed?
-	//	if (*pSpecialKey & 0x2)
-	//		*pModifier |= HidLCtrlMask; // Set LCtrl modifier
+	//	if (buf->special_key & 0x2)
+	//		buf->modifiers |= HidLCtrlMask; // Set LCtrl modifier
 	//	else
-	//		*pModifier &= ~HidLCtrlMask; // Clear LCtrl modifier
+	//		buf->modifiers &= ~HidLCtrlMask; // Clear LCtrl modifier
 	//}
 	//else // Not SwapFnCtrl mode
-	BOOLEAN prev_fn = g_FakeFnActive;
-	g_FakeFnActive = *pSpecialKey & 0x2; // Set FakeFnActive state based on physical Fn key state
+	BOOLEAN prev_fn = g_FnKeyActive;
+	g_FnKeyActive = buf->special_key & 0x2; // Set FakeFnActive state based on physical Fn key state
 
-	// fn released - prevent immediate accidental keystokes by clearing the buffer
-	if ((prev_fn != g_FakeFnActive) && !g_FakeFnActive) {
-		*pKey1 = 0;
-		*pKey2 = 0;
-		*pKey3 = 0;
-		*pKey4 = 0;
-		*pKey5 = 0;
-		*pKey6 = 0;
-		*pSpecialKey = 0;
+	// Fn has just been released, so prevent immediate accidental keystokes by clearing the buffer.
+	if ((prev_fn != g_FnKeyActive) && !g_FnKeyActive) {
+		buf->keys[0] = 0;
+		buf->keys[1] = 0;
+		buf->keys[2] = 0;
+		buf->keys[3] = 0;
+		buf->keys[4] = 0;
+		buf->keys[5] = 0;
+		buf->special_key = 0;
 	}
 
 	// Eject Pressed?
-	if (*pSpecialKey & 0x1 && g_dwEjectScanCode <= 0xff)
-		*pKey6 = HidDel; // Avoid overriding key like caps lock by using last (6th) slot
+	if (buf->special_key & 0x1 && g_dwEjectScanCode <= 0xff) {
+		buf->keys[5] = HidDel; // Avoid overriding key like caps lock by using last (6th) slot.
+	}
 
-	*pSpecialKey = 0; //Clear special key
+	buf->special_key = 0;
 
 	// Alt-Cmd swap
 
-	if (*pModifier & HidLAltMask)
-	{
-		*pModifier &= ~HidLAltMask;
-		*pModifier |= HidLCmdMask;
+	if (buf->modifiers & HidLAltMask) {
+		buf->modifiers &= ~HidLAltMask;
+		buf->modifiers |= HidLCmdMask;
 	}
-	else if (*pModifier & HidLCmdMask)
-	{
-		*pModifier &= ~HidLCmdMask;
-		*pModifier |= HidLAltMask;
+	else if (buf->modifiers & HidLCmdMask) {
+		buf->modifiers &= ~HidLCmdMask;
+		buf->modifiers |= HidLAltMask;
 	}
 
-	if (*pModifier & HidRAltMask)
-	{
-		*pModifier &= ~HidRAltMask;
-		*pModifier |= HidRCtrlMask;
+	if (buf->modifiers & HidRAltMask) {
+		buf->modifiers &= ~HidRAltMask;
+		buf->modifiers |= HidRCtrlMask;
 	}
-	else if (*pModifier & HidRCmdMask)
-	{
-		*pModifier &= ~HidRCmdMask;
-		*pModifier |= HidRAltMask;
+	else if (buf->modifiers & HidRCmdMask) {
+		buf->modifiers &= ~HidRCmdMask;
+		buf->modifiers |= HidRAltMask;
 	}
 	
-	// Process FakeFn+[key] combination 
-	if (g_FakeFnActive && (*pKey1 || *pModifier))
-	{
-		switch (*pKey1)
-		{
-		case HidLeft: *pKey1 = HidHome; break;
-		case HidRight: *pKey1 = HidEnd; break;
-		case HidUp: *pKey1 = HidPgUp; break;
-		case HidDown: *pKey1 = HidPgDown; break;
-		case HidEnter: *pKey1 = HidInsert; break;
-		case HidCapsLock: *pKey1 = HidCapsLock; break;
-		case HidF1: *pKey1 = HidF13; break;
-		case HidF2: *pKey1 = HidF14; break;
-		case HidF3: *pKey1 = HidF15; break;
-		case HidF4: *pKey1 = HidF16; break;
-		case HidF5: *pKey1 = HidF17; break;
-		case HidF6: *pKey1 = HidF18; break;
-		case HidF7: *pKey1 = HidF19; break;
-		case HidF8: *pKey1 = HidF20; break;
-		case HidF9: *pKey1 = HidF21; break;
-		case HidF10: *pKey1 = HidF22; break;
-		case HidF11: *pKey1 = HidF23; break;
-		case HidF12: *pKey1 = HidF24; break;
-		case HidKeyP: *pKey1 = HidPrtScr; break;
-		case HidKeyB: *pKey1 = HidPauseBreak; break;
-		//case HidKeyS: *pKey1 = HidScrLck; break;
-		default:
-			//if (*pModifier & HidLCtrlMask) // Map Fn+LCtrl to RCtrl
-			//{
-			//	*pModifier &= ~HidLCtrlMask; // Clear LCtrl
-			//	*pModifier |= HidRCtrlMask; // Set RCtrl
-			//}
-			//else
-			RtlZeroMemory(buf, size); // Ignore all other Fn+[key] combinations
-			break;
+	// Process Fn key combinations
+	if (g_FnKeyActive) {
+		for (int idx = 0; idx < 6; idx++) {
+			BYTE key2;
+			switch (buf->keys[idx]) {
+			case HidLeft: key2 = HidHome; break;
+			case HidRight: key2 = HidEnd; break;
+			case HidUp: key2 = HidPgUp; break;
+			case HidDown: key2 = HidPgDown; break;
+			case HidEnter: key2 = HidInsert; break;
+			case HidF1: key2 = HidF13; break;
+			case HidF2: key2 = HidF14; break;
+			case HidF3: key2 = HidF15; break;
+			case HidF4: key2 = HidF16; break;
+			case HidF5: key2 = HidF17; break;
+			case HidF6: key2 = HidF18; break;
+			case HidF7: key2 = HidF19; break;
+			case HidF8: key2 = HidF20; break;
+			case HidF9: key2 = HidF21; break;
+			case HidF10: key2 = HidF22; break;
+			case HidF11: key2 = HidF23; break;
+			case HidF12: key2 = HidF24; break;
+			case HidKeyP: key2 = HidPrtScr; break;
+			case HidKeyB: key2 = HidPauseBreak; break;
+			//case HidKeyS: key2 = HidScrLck; break;
+			case HidCapsLock: key2 = HidCapsLock; break;
+			default:
+				key2 = 0; // Ignore this key
+				break;
+			}
+			buf->keys[idx] = key2;
 		}
 	}
-	
 }
